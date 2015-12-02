73a21b9e4cfd66bb7b2ff83d3cb57a59ce22206a
aserafin <adrian@softmad.pl>
2015-12-02 19:30:43 +0100

    you can now reload! in the console :)

--- config/application.rb
+++ config/application.rb
@@ -6,41 +6,42 @@ ENV['RUNNER_ENV'] ||= 'development'
 Dotenv.load
 
 #models
-require_relative '../app/models/assertion'
-require_relative '../app/models/data_point'
-require_relative '../app/models/request'
-require_relative '../app/models/test'
+load File.expand_path('../app/models/assertion.rb', __FILE__)
+load File.expand_path('../app/models/assertion.rb', __FILE__)
+load File.expand_path('../app/models/data_point.rb', __FILE__)
+load File.expand_path('../app/models/request.rb', __FILE__)
+load File.expand_path('../app/models/test.rb', __FILE__)
 
 # services
-require_relative '../app/services/assertion_tester'
-require_relative '../app/services/condition_factory'
-require_relative '../app/services/data_point_collector'
-require_relative '../app/services/extractor_factory'
-require_relative '../app/services/request_builder'
-require_relative '../app/services/request_executor'
-require_relative '../app/services/test_runner'
+load File.expand_path('../app/services/assertion_tester.rb', __FILE__)
+load File.expand_path('../app/services/condition_factory.rb', __FILE__)
+load File.expand_path('../app/services/data_point_collector.rb', __FILE__)
+load File.expand_path('../app/services/extractor_factory.rb', __FILE__)
+load File.expand_path('../app/services/request_builder.rb', __FILE__)
+load File.expand_path('../app/services/request_executor.rb', __FILE__)
+load File.expand_path('../app/services/test_runner.rb', __FILE__)
 
 # extractors
-require_relative '../app/extractors/base'
-require_relative '../app/extractors/header'
-require_relative '../app/extractors/json'
-require_relative '../app/extractors/response_time'
-require_relative '../app/extractors/status_code'
-require_relative '../app/extractors/text'
-require_relative '../app/extractors/xml'
+load File.expand_path('../app/extractors/base.rb', __FILE__)
+load File.expand_path('../app/extractors/header.rb', __FILE__)
+load File.expand_path('../app/extractors/json.rb', __FILE__)
+load File.expand_path('../app/extractors/response_time.rb', __FILE__)
+load File.expand_path('../app/extractors/status_code.rb', __FILE__)
+load File.expand_path('../app/extractors/text.rb', __FILE__)
+load File.expand_path('../app/extractors/xml.rb', __FILE__)
 
-#consitions
-require_relative '../app/conditions/base'
-require_relative '../app/conditions/contain'
-require_relative '../app/conditions/empty'
-require_relative '../app/conditions/equal'
-require_relative '../app/conditions/greater_than'
-require_relative '../app/conditions/greater_than_or_equal'
-require_relative '../app/conditions/has_key'
-require_relative '../app/conditions/has_value'
-require_relative '../app/conditions/is_null'
-require_relative '../app/conditions/less_than'
-require_relative '../app/conditions/less_than_or_equal'
-require_relative '../app/conditions/not_contain'
-require_relative '../app/conditions/not_empty'
-require_relative '../app/conditions/not_equal'
+#conditions
+load File.expand_path('../app/conditions/base.rb', __FILE__)
+load File.expand_path('../app/conditions/contain.rb', __FILE__)
+load File.expand_path('../app/conditions/empty.rb', __FILE__)
+load File.expand_path('../app/conditions/equal.rb', __FILE__)
+load File.expand_path('../app/conditions/greater_than.rb', __FILE__)
+load File.expand_path('../app/conditions/greater_than_or_equal.rb', __FILE__)
+load File.expand_path('../app/conditions/has_key.rb', __FILE__)
+load File.expand_path('../app/conditions/has_value.rb', __FILE__)
+load File.expand_path('../app/conditions/is_null.rb', __FILE__)
+load File.expand_path('../app/conditions/less_than.rb', __FILE__)
+load File.expand_path('../app/conditions/less_than_or_equal.rb', __FILE__)
+load File.expand_path('../app/conditions/not_contain.rb', __FILE__)
+load File.expand_path('../app/conditions/not_empty.rb', __FILE__)
+load File.expand_path('../app/conditions/not_equal.rb', __FILE__)
